9904d02d461c768257e1810070e515628502cb6b
Victoria Beasley <iesnoth91@gmail.com>
2023-03-18 20:44:03 -0500

    starting sign in

---
+++ frontend/src/components/CreateUser.jsx
@@ -0,0 +1,15 @@
+import React, {Fragment} from "react";
+
+const CreateUser = () => {
+    return(
+        <Fragment>
+            <h1>Sign up!</h1>
+            <form>
+                <input type= "text"/>
+                <button>Submit</button>
+            </form>
+        </Fragment>
+    )
+}
+
+export default CreateUser;
