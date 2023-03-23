6ae8c90dff26c131d740af289d960397904f7cbf
Victoria Beasley <iesnoth91@gmail.com>
2023-03-22 20:40:48 -0500

    trying to debug dragon create route

--- backend/controllers/dragon_controller.js
+++ backend/controllers/dragon_controller.js
@@ -7,12 +7,12 @@ const { User, Pet } = db
 
 //CREATE a dragon
 const createDragon = asyncHandler(async (req, res) => {
-    const { userUuid, name, images, price, artist, description } = req.body
+    const { userUuid, name, images, price, description } = req.body
 
     try {
         const user = await User.findOne({ where: { user_uid: userUuid } })
 
-        const newDragon = Pet.create({ name, images, price, artist, description, artistId: user.id })
+        const newDragon = Pet.create({ name, images, price, description, artistId: user.id })
         return res.json(newDragon)
     } catch (err) {
         console.log(err)
