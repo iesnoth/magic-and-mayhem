f6f771b1d0e98f48496c53c456c699cca2e83764
George <georgehender997@gmail.com>
2023-03-26 12:53:44 -0400

    done with slice

--- frontend/src/features/dragons/dragonSlice.jsx
+++ frontend/src/features/dragons/dragonSlice.jsx
@@ -1,1 +1,164 @@
 //do we need this?
+
+import {createSlice, createAsyncThunk} from '@reduxjs/toolkit'
+import dragonService from './dragonService'
+
+const initialState = {
+    dragons: [],
+    isError: false,
+    isSuccess: false,
+    isLoading: false,
+    message: '',
+}
+
+//get all dragons
+export const getDragons = createAsyncThunk(
+    'dragons/getAll',
+    async (thunkAPI) => {
+        try {
+            return await dragonsService.getDragons()
+        } catch (error) {
+            const message = 
+                (error.response &&
+                    error.response.data &&
+                    error.response.data.message) ||
+                error.message ||
+                error.toString()
+            return thunkAPI.rejectWithValue(message)
+        }
+    }
+)
+
+//create new dragon
+
+export const createDragon = createAsyncThunk(
+    'dragons/create',
+
+    async (dragonsData, thunkAPI) => {
+        try {
+            const token = thunkAPI.getState().auth.user.token
+            return await dragonsService.createDragon(dragonsData, token)
+        } catch (error) {
+            const message = 
+                (error.response &&
+                    error.response.data &&
+                    error.response.data.message) ||
+                error.message ||
+                error.toString()
+            return thunkAPI.rejectWithValue(message)
+        }
+    }
+
+)
+
+//get user specific dragons
+
+export const getMyDragons = createAsyncThunk('dragons/getMine', 
+    async (_, thunkAPI) => {
+        try {
+            const token = thunkAPI.getState().auth.user.token
+            return await dragonService.getMyDragons(token)
+        } catch (error) {
+            const message = 
+                (error.response &&
+                    error.response.data &&
+                    error.response.data.message) ||
+                error.message ||
+                error.toString()
+            return thunkAPI.rejectWithValue(message)
+        }
+    })
+
+//Delete dragon
+
+export const deleteDragon = createAsyncThunk(
+    'dragons/delete',
+    async (id, thunkAPI) => {
+        try {
+            const token = thunkAPI.getState().auth.user.token
+            return await dragonsService.deleteDragon(id, token)
+        } catch (error) {
+            const message = 
+                (error.response &&
+                    error.response.data &&
+                    error.response.data.message) ||
+                error.message ||
+                error.toString()
+            return thunkAPI.rejectWithValue(message)
+        }
+    }
+)
+
+export const dragonSlice = createSlice({
+    name: 'dragons',
+    initialState,
+    reducers: {
+        reset: (state) => initialState,
+    },
+    extraReducers: (builder) => {
+        builder
+
+            .addCase(createDragon.pending, (state) => {
+                state.isLoading = true
+            })
+            .addCase(createDragon.fulfilled, (state, action) => {
+                state.isLoading = false
+                state.isSuccess = true
+                state.dragons = action.payload
+            })
+            .addCase(createDragon.rejected, (state, action) => {
+                state.isLoading = false
+                state.isError = true
+                state.message = action.payload
+            })
+
+            //get user soecific dragons
+            .addCase(getMyDragons.pending, (state) => {
+                state.isLoading = true
+            })
+            .addCase(getMyDragons.fulfilled, (state, action) => {
+                state.isLoading = false
+                state.isSuccess = true
+                state.dragons = action.payload
+            })
+            .addCase(getMyDragons.rejected, (state, action) => {
+                state.isLoading = false
+                state.isError = true
+                state.message = action.payload
+            })
+
+            //end user specific slice settings
+
+            .addCase(getDragons.pending, (state) => {
+                state.isLoading = true
+            })
+            .addCase(getDragons.fulfilled, (state, action) => {
+                state.isLoading = false 
+                state.isSuccess = true
+                state.dragons = action.payload 
+            })
+            .addCase(getDragons.rejected, (state, action) => {
+                state.isLoading = false
+                state.isError = true
+                state.message = action.payload 
+            })
+            .addCase(deleteDragon.pending, (state) => {
+                state.isLoading = true
+            })
+            .addCase(deleteDragon.fulfilled, (state, action) => {
+                state.isLoading = false
+                state.isSuccess = true
+                state.dragons = state.dragons.filter(
+                    (dragon) => dragons._id !== action.payload.id
+                )
+            })
+            .addCase(deleteDragon.rejected, (state, action) => {
+                state.isLoading = false
+                state.isError = true
+                state.message = action.payload
+            })
+    },
+})
+
+export const { reset } = dragonSlice.actions
+export default dragonSlice.reducer
--- frontend/src/pages/CreateUpdateDragon.jsx
+++ frontend/src/pages/CreateUpdateDragon.jsx
@@ -3,11 +3,80 @@
 //images, price, and a short description of the dragon
 //After being submitted, the dragon is turned into an EachDragon component and sent to the Gallery and the vender's account page.
 
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 function CreateUpdateDragon() {
+
+const [formData, setFormData] = useState({
+    nameDragon: '',
+    nameArtist: '',
+    color: '',
+    style: '',
+    images: '',
+    price: '',
+    description: '',
+})
+
+const { nameDragon, nameArtist, color, style, images, price, description } = formData
+
+const navigate = useNavigate()
+const dispatch = useDispatch()
+
+const { dragon, isError, isSuccess, message } = useSelector(
+    (state) => state.dragon
+)
+
+useEffect(() => {
+    if (isError) {
+        toast.error(message)
+    }
+
+    if (isSuccess || dragon) {
+
+    }
+
+    dispatch(reset())
+}, [dragon, isError, isSuccess, message, navigate, dispatch])
+
+const onChange = (e) => {
+    setFormData((prevState) => ({
+        ...prevState,
+        [e.target.name]: e.target.value,
+    }))
+}
+
+const handleSubmit = (event) => {
+    event.preventDefault();
+}
+
+if (!nameDragon || !nameArtist || !color || !style || !images || !price || !description) {
+    toast.error('Populate all required fields')
+} else {
+    const dragonData = {
+        nameDragon, nameArtist, color, style, images, price, description
+    }
+
+    dispatch(createDragon(dragonData))
+    toast.success('New Dragon successfully added!')
+    navigate('/')
+}
+
+
     return (
-        <h1>This is the Create post page</h1>
+          <>
+          <div>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <h1>Add your Dragon</h1>
+                    <div>
+                        <label htmlFor="nameDragon">Name of dragon</label>
+                        <input type="text" name="" id="" />
+                    </div>
+                </div>
+            </form>
+          </div>
+          </>
     )
     
 }
