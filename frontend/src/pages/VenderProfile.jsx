c78c91da3407a5bbf888bb22f4a8c15ebcbad36a
Victoria Beasley <iesnoth91@gmail.com>
2023-03-29 10:50:20 -0500

    updating

--- frontend/src/pages/VenderProfile.jsx
+++ frontend/src/pages/VenderProfile.jsx
@@ -1,28 +1,28 @@
-//There are 2 kinds of profile, based on roles: buyer and vender
-//Buyer Profile
-//Has a list of dragons the buyer has bought. They can look at their specs upon clicking them.
-//navbar on the side that has: payment options, pets, and account settings
-//NOTE: if they have been bought, they are no longer in the store front. Which db table would they be in?
-//Vender Profile WE WILL FOCUS ON BUILDING OUT THIS PROFILE:
-//Has everything the Buyer Profile has, as well as:
-//a page for them to view all the dragons they've put up for sale, and give them an option to edit/delete unsold dragons
-
 import React, { useEffect } from "react";
 import { useNavigate } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux'
+import VendorView from "../components/VendorView";
+import { getMyDragons, reset } from "../features/dragons/dragonSlice";
 
 function VenderProfile() {
     const navigate = useNavigate()
     const dispatch = useDispatch()
 
     const { user } = useSelector((state) => state.auth)
+    const { dragons, isError, isLoading, message } = useSelector((state) => state.dragons)
 
     useEffect(() => {
-        // if (isError) {
-        //     console.log(message)
-        // }
+        if (isError) {
+            console.log(message)
+        }
         if (!user) {
             navigate('/')
+        }
+
+        dispatch(getMyDragons())
+
+        return () => {
+            dispatch(reset)
         }
     }, [user])
 
@@ -40,10 +40,23 @@ function VenderProfile() {
                         <p>You are a buyer</p>
 
                     </>) : (<>
-                        <p>You are a vendor</p>
-                        <p>Here are the dragons you are selling.</p>
+                        <section>
+                            <h3>Here are the dragons you are selling:</h3>
+                            {dragons.length > 0 ? (
+                                <ul className="cards">
+                                    {dragons.map((dragons) => (
+                                        <VendorView key={dragons.pet_uid} dragon={dragons} />
+                                    ))}
+                                </ul>
+                            ):(
+                                <h3>You are not currently selling any dragons. Shame on you.</h3>
+                            )}
+                        </section>
                     </>)}
-                <p>Here are the dragons you have bought</p>
+                    {/* Adopted dragons section */}
+                <section>
+                    
+                </section>
             </div>
         </>
     )
